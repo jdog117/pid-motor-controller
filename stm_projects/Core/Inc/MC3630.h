89ca3a49813772814aa52ddc75602399aacdbb2a
Joseph Casey <jspcasey@gmail.com>
2023-10-08 13:47:11 -0500

    added source and header file for accelerometer

---
+++ stm_projects/Core/Inc/MC3630.h
@@ -0,0 +1,13 @@
+/*
+ * MC3630.h
+ *
+ *  Created on: Oct 8, 2023
+ *      Author: jspca
+ */
+
+#ifndef INC_MC3630_H_
+#define INC_MC3630_H_
+
+
+
+#endif /* INC_MC3630_H_ */
---
+++ stm_projects/Core/Src/MC3630.c
@@ -0,0 +1,8 @@
+/*
+ * MC3630.c
+ *
+ *  Created on: Oct 8, 2023
+ *      Author: jspca
+ */
+
+
