f03c5b6300ca5cc58886909e274a6efb2ac41791
Joseph Casey <jspcasey@gmail.com>
2023-10-09 11:29:22 -0500

    starting pid operation fucntions

--- stm_projects/Core/Src/pid.c
+++ stm_projects/Core/Src/pid.c
@@ -5,5 +5,32 @@
  *      Author: jspca
  */
 
+/*
+ *        OPERATION FUCTIONNS
+ */
 
+/* ::::::::::: Computing ::::::::::: */
+uint8_t PID_Compute(PID_TypeDef *uPID);
 
+/* ::::::::::: PID Mode :::::::::::: */
+void            PID_SetMode(PID_TypeDef *uPID, PIDMode_TypeDef Mode);
+PIDMode_TypeDef PID_GetMode(PID_TypeDef *uPID);
+
+/* :::::::::: PID Limits ::::::::::: */
+void PID_SetOutputLimits(PID_TypeDef *uPID, double Min, double Max);
+
+/* :::::::::: PID Tunings :::::::::: */
+void PID_SetTunings(PID_TypeDef *uPID, double Kp, double Ki, double Kd);
+void PID_SetTunings2(PID_TypeDef *uPID, double Kp, double Ki, double Kd, PIDPON_TypeDef POn);
+
+/* ::::::::: PID Direction ::::::::: */
+void          PID_SetControllerDirection(PID_TypeDef *uPID, PIDCD_TypeDef Direction);
+PIDCD_TypeDef PID_GetDirection(PID_TypeDef *uPID);
+
+/* ::::::::: PID Sampling :::::::::: */
+void PID_SetSampleTime(PID_TypeDef *uPID, int32_t NewSampleTime);
+
+/* ::::::: Get Tunings Param ::::::: */
+double PID_GetKp(PID_TypeDef *uPID);
+double PID_GetKi(PID_TypeDef *uPID);
+double PID_GetKd(PID_TypeDef *uPID);
