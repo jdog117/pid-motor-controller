45a5885f847234c22ab088463c186a8ba0c08ef5
Joseph Casey <jspcasey@gmail.com>
2023-10-12 23:59:26 -0500

    writing register functions

--- stm_projects/Core/Src/MC3630.c
+++ stm_projects/Core/Src/MC3630.c
@@ -8,13 +8,21 @@
  */
 
 #include "MC3630.h"
-#include "stm32c0xx_hal.h"
 
 
 HAL_StatusTypeDef MC3630_ReadRegiser(MC3630 *dev, uint8_t reg, uint8_t *data) {
-	return HAL_I2C_Mem_Read(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout)
+	//DevAddress is the device address shifted by 1
+	//reads 1 byte of data
+	return HAL_I2C_Mem_Read(dev->i2cHandle, MC3630_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef MC3630_ReadRegisers(MC3630 *dev, uint8_t reg, uint8_t *data, uint8_t length) {
+	//DevAddress is the device address shifted by 1
+	//reads 1 byte of data
+	return HAL_I2C_Mem_Read(dev->i2cHandle, MC3630_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, length, HAL_MAX_DELAY);
 }
 
 
-
-
+HAL_StatusTypeDef MC3630_WriteRegiser(MC3630 *dev, uint8_t reg, uint8_t *data) {
+	return HAL_I2C_Mem_Write(dev->i2cHandle, MC3630_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+}
